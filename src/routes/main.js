5ffbb4c4c7e585cb79c26868eb690e5bfc2c3c7b
Aaron8027 <77461333+Aaron8027@users.noreply.github.com>
2022-02-06 00:20:44 -0600

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,9 +3,9 @@ const router = Router();
 
 router.get('/', (require,res)=>{ //ruta
     const datos = {
-        "id": "1",
-        "nombre": "Cesar",
-        "telefono": "557772"
+        "id": "508695",
+        "nombre": "Elsa Badillo",
+        "telefono": "5577724863"
     }
     res.json(datos);
 });
